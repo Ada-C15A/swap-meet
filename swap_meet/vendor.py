e267f66a5d2120eca0bcd9a41efca62ad75a8a68
Christina <206peacegypsy@gmail.com>
2021-05-18 09:26:20 -0700

    wave o2, all tests passing

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,7 +1,10 @@
+from swap_meet.item import Item
+
 class Vendor:
     
-    def __init__(self, inventory=[]):
+    def __init__(self, inventory=[], items=[]):
         self.inventory = inventory
+        self.items = items 
         
     def add(self, item_to_add):
         self.inventory.append(item_to_add)
@@ -14,5 +17,10 @@ class Vendor:
         else:
             return False
         
-    
+    def get_by_category(self, category):
+        item_list = []
+        for item in self.inventory:
+            if item.category == category:
+                item_list.append(item)
+        return item_list
     
