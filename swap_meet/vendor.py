7c00acaf0087b20306851c0a36f2dcaad48f767f
Christina <206peacegypsy@gmail.com>
2021-05-17 18:55:08 -0700

    wave 01 all tests passing

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,18 @@
 class Vendor:
-    pass
+    
+    def __init__(self, inventory=[]):
+        self.inventory = inventory
+        
+    def add(self, item_to_add):
+        self.inventory.append(item_to_add)
+        return item_to_add
+    
+    def remove(self, item_to_remove):
+        if item_to_remove in  self.inventory:
+            self.inventory.remove(item_to_remove)
+            return item_to_remove
+        else:
+            return False
+        
+    
+    
