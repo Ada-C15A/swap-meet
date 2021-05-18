e2ef0816bdcab725f58bd2481f0ebc59d0a7a56e
RETURNcaden <cadence1547@gmail.com>
2021-05-18 15:42:09 -0700

    pass wave_01

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,14 @@
 class Vendor:
-    pass
+    def __init__(self,inventory=[]):
+        self.inventory=inventory 
+    
+    def add(self,item):
+        self.inventory.append(item)
+        return self.inventory[len(self.inventory)-1]
+
+    def remove(self,item):
+        for i in self.inventory:
+            if i == item:
+                self.inventory.remove(item)
+                return item
+        return False
