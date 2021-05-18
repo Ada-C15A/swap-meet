695af590a977f8b95f13f282d826ea8aa97bffe0
Dena Burd <denaburd@gmail.com>
2021-05-17 20:53:54 -0700

    all tests passing

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,2 +1,8 @@
-class Electronics:
-    pass
+from swap_meet.item import Item
+
+class Electronics(Item):
+    def __init__(self, condition=0, age=1):
+        Item.__init__(self, condition, age, "Electronics")
+
+    def __str__(self):
+        return("A gadget full of buttons and secrets.")
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,78 @@
+from operator import attrgetter
 class Vendor:
-    pass
+    def __init__(self, inventory = []):
+        self.inventory = inventory
+
+    def add(self,item):
+        self.inventory.append(item)
+        return item
+
+    def remove(self,item):
+        if item in self.inventory:
+            self.inventory.remove(item)
+            return item
+        else:  return False
+    
+    def get_by_category(self,category):
+        category_items = []
+        for item in self.inventory:
+            if item.category == category:
+                category_items.append(item)
+        return category_items
+
+    def swap_items(self, vendor, my_item, their_item):
+        if my_item in self.inventory and their_item in vendor.inventory:
+            self.remove(my_item)
+            self.add(their_item)
+            vendor.remove(their_item)
+            vendor.add(my_item)
+            return True
+        else: return False
+    
+    def swap_first_item(self, vendor):
+        if len(self.inventory) > 0 and len(vendor.inventory) > 0:
+            my_item = self.inventory[0]
+            their_item = vendor.inventory[0]
+            self.swap_items(vendor, my_item, their_item)
+            return True
+        else: return False
+
+    def get_best_by_category(self, category):
+        items = [item for item in self.inventory if item.category == category]
+        if len(items) > 0:
+            best_item = items[0]
+            for item in items:
+                if item.condition > best_item.condition:
+                    best_item = item
+            return best_item
+        else: return None
+    
+    def swap_best_by_category(self, other, my_priority, their_priority):
+        my_best = self.get_best_by_category(their_priority)
+        their_best = other.get_best_by_category(my_priority)
+        if my_best and their_best:
+            self.swap_items(other, my_best, their_best)
+            return True
+        else: return False
+
+    def get_newest_by_category(self,category=None):
+        if category == None:
+            items = self.inventory
+        else:
+            items = [item for item in self.inventory if item.category == category]
+        if len(items) > 0:
+            newest = None
+            for item in items:
+                if not newest or item['age'] < newest['age']:
+                    newest = item
+            return newest
+        else: return False
+
+    def swap_newest_by_category(self, other, category=None):
+        if len(self.inventory) > 0 and len(other.inventory) > 0:
+            my_newest = self.get_newest_by_category(category)
+            their_newest = other.get_newest_by_category(category)
+            if my_newest and their_newest:
+                self.swap_items(other, my_newest, their_newest)
+            else: return False
+        else: return False
