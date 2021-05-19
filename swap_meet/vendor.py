a8de8802f1a8c959b374a40f45d7a1ae8b45ad5a
RETURNcaden <cadence1547@gmail.com>
2021-05-18 17:17:28 -0700

    pass wave_02

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,7 +1,7 @@
 class Vendor:
     def __init__(self,inventory=[]):
         self.inventory=inventory 
-    
+       
     def add(self,item):
         self.inventory.append(item)
         return self.inventory[len(self.inventory)-1]
@@ -12,3 +12,14 @@ class Vendor:
                 self.inventory.remove(item)
                 return item
         return False
+    
+    def get_by_category(self,accessory):
+        print(self.inventory)
+        matching_list=[]
+        for i in self.inventory:
+            print("jj",i)
+            if i.category == accessory:
+                matching_list.append(i)
+        print("see",matching_list)
+        return matching_list
+    
