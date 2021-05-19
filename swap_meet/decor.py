e09e5bfea0877b6ce451ea8fdb1eeb570549397e
trish <pmgriego@gmail.com>
2021-05-18 22:14:12 -0700

    access item condition in child classes

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,8 +1,8 @@
 from swap_meet.item import Item
 
 class Decor(Item):
-    def __init__(self, category="Decor"):
-        self.category = category
+    def __init__(self, category="Decor", condition=0.0):
+        super().__init__(category, condition)
 
     def __str__(self):
         return "Something to decorate your space."
