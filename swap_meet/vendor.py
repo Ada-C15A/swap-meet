9e2fd7d2ac7aae3d20444b07db98c4adc867e6cb
kaylaecker <kayjaysrainbows@gmail.com>
2021-05-18 22:14:25 -0700

    passes all waves

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -38,3 +38,23 @@ class Vendor:
             self.inventory[0],
         ]
         return True
+
+    def get_best_by_category(self, item_category):
+        applicable_items = self.get_by_category(item_category)
+
+        if not applicable_items:
+            return None
+
+        highest_item = applicable_items[0]
+
+        for current_item in applicable_items:
+            if highest_item.condition < current_item.condition:
+                highest_item = current_item
+
+        return highest_item
+
+    def swap_best_by_category(self, other, my_priority, their_priority):
+        my_best = self.get_best_by_category(their_priority)
+        their_best = other.get_best_by_category(my_priority)
+
+        return self.swap_items(other, my_best, their_best)
