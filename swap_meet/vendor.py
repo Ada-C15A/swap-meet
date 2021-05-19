25e50a356d7e5c6c9f912aace426b9f62f96d7ab
Cloudy Lopez <CloudyLopez@icloud.com>
2021-05-19 15:49:03 -0700

    wave three completed - all tests passing

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -20,3 +20,13 @@ class Vendor:
             if item.category == category:
                 category_items.append(item)
         return category_items
+
+    def swap_items(self, vendor, my_item, their_item):
+        if my_item in self.inventory and their_item in vendor.inventory:
+            self.inventory.remove(my_item) #takes my item
+            self.inventory.append(their_item) #adds it to theirs
+            vendor.inventory.remove(their_item) #removes theirs
+            vendor.inventory.append(my_item) #adds it to mine
+            return True 
+        else:
+            return False
