dd1db6e56f96bd8d35d296041780de07bfdd1765
janicelichtman <jalichtman@gmail.com>
2021-05-19 00:30:14 -0700

    Wave 6 passes tests

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,3 +1,5 @@
+from operator import itemgetter
+
 class Vendor:
     
     def __init__(self, inventory=[]):
@@ -17,23 +19,41 @@ class Vendor:
     def get_by_category(self, category):
         return [item for item in self.inventory if item.category ==  category]
     
-    def swap_items(self, vendor, my_item, their_item):
-        if my_item not in self.inventory or their_item not in vendor.inventory:
+    def swap_items(self, other, my_item, their_item):
+        if my_item not in self.inventory or their_item not in other.inventory:
             return False
         else:
             self.inventory.remove(my_item)
             self.inventory.append(their_item)
-            vendor.inventory.remove(their_item)
-            vendor.inventory.append(my_item)
+            other.inventory.remove(their_item)
+            other.inventory.append(my_item)
             return True
     
-    def swap_first_item(self, vendor):
-        if not self.inventory or not vendor.inventory:
+    def swap_first_item(self, other):
+        if not self.inventory or not other.inventory:
             return False
         else:
-            my_first = self.inventory.pop(0)
-            their_first = vendor.inventory.pop(0)
-            self.inventory.append(their_first)
-            vendor.inventory.append(my_first)
-            return True
+            my_first = self.inventory[0]
+            their_first = other.inventory[0]
+            return self.swap_items(other, my_first, their_first)
+
+    def get_best_by_category(self, category):
+        category_matches = [(item, item.condition) for item in self.inventory if item.category == category]
+        if not category_matches:
+            return None
+        else:
+            # Using itemgetter: This returns a callable to be used on an iterable object, and returns 
+            # the specified value from the iterable.             
+            # Also note that the 'key' parameter of max allows us to specify a function whose return values 
+            # will be used for the comparison
+            best = max(category_matches, key = itemgetter(1))[0]
+            return best 
+
+    def swap_best_by_category(self, other, my_priority, their_priority):
+        my_item = self.get_best_by_category(their_priority) 
+        their_item = other.get_best_by_category(my_priority)
         
+        if not my_item or not their_item:
+            return False
+        else:
+            return self.swap_items(other, my_item, their_item)
