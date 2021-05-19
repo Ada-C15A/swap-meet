0e78ce55108fe04ced5a1b5590d718a031a7e70f
janicelichtman <jalichtman@gmail.com>
2021-05-18 21:10:51 -0700

    Waves 1 ,2 ,and 3 pass tests

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,28 @@
 class Vendor:
-    pass
+    
+    def __init__(self, inventory=[]):
+        self.inventory = inventory
+
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+
+    def remove(self, item):
+        if item in self.inventory:
+            self.inventory.remove(item)
+            return item
+        else:
+            return False
+            
+    def get_by_category(self, category):
+        return [item for item in self.inventory if item.category ==  category]
+    
+    def swap_items(self, vendor, my_item, their_item):
+        if my_item not in self.inventory or their_item not in vendor.inventory:
+            return False
+        else:
+            self.inventory.remove(my_item)
+            self.inventory.append(their_item)
+            vendor.inventory.remove(their_item)
+            vendor.inventory.append(my_item)
+            return True
