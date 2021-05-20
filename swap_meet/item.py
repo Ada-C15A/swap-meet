c3aae033c6e993bd488abdbe4599de8e921a2be4
denisseai <msdenisseanaya@gmail.com>
2021-05-20 10:27:14 -0700

    wave 5 tests passing

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,6 +1,12 @@
 class Item:
-    def __init__(self, category = ""):
+    def __init__(self, category = "", condition=0):
         self.category = category
+        self.condition = condition
 
     def __str__(self):
         return "Hello World!"
+    
+    def condition_description(self):
+        descriptions = ["Trash","Used","Gently Used","Like New","New", "New Tags"]
+        return descriptions[self.condition]
+        
