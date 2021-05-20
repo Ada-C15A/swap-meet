fdb5e5f65ca5e884d58af33140e8cda99e175d12
Lilia <lpla1154@gmail.com>
2021-05-20 16:14:38 -0400

    wave 1-5

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,21 @@
 class Item:
-    pass
+    def __init__(self, category = '', condition = 0):
+        self.category = category
+        self.condition = condition
+
+    def __str__(self):
+        return "Hello World!"
+
+    def condition_description(self):
+        if self.condition == 0:
+            return "Acceptable"
+        elif self.condition == 1:
+            return "Functional"
+        elif self.condition == 2:
+            return "Very good"
+        elif self.condition == 3:
+            return "A fresh home for tired eyes"
+        elif self.condition == 4:
+            return "It's new to you"
+        elif self.condition == 5:
+            return "Amazing!"
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,51 @@
 class Vendor:
-    pass
+    def __init__(self, inventory=[]):
+        self.inventory = inventory
+
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+
+    def remove(self, item):
+        if item not in self.inventory:
+            return False
+
+        self.inventory.remove(item)
+        return item
+
+    def get_by_category(self, category):
+        category_items = []
+
+        for item in self.inventory:
+            if item.category ==  category:
+                category_items.append(item)
+        return category_items
+
+    def swap_items(self, vendor, my_item, their_item):
+        if my_item not in self.inventory or their_item not in vendor.inventory:
+            return False
+        else:
+            self.inventory.remove(my_item)
+            self.inventory.append(their_item)
+            vendor.inventory.remove(their_item)
+            vendor.inventory.append(my_item)
+            return True 
+    
+    def swap_first_item(self, vendor):
+        if len(self.inventory) == 0 or len(vendor.inventory) == 0:
+            return False
+        else:
+            self.swap_items(vendor, self.inventory[0], vendor.inventory[0])
+            return True
+    
+    def get_best_by_category(self, category_str):
+        highest_so_far = None
+
+        for item in self.inventory:
+            if item.category == category_str:
+                if highest_so_far:
+                    if item.condition > highest_so_far.condition:
+                        highest_so_far = item
+                else:
+                    highest_so_far = item
+        return highest_so_far
