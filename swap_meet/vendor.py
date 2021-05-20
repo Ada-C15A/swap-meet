7e4157752ca192a9592431b4b49ae17f33724878
RETURNcaden <cadence1547@gmail.com>
2021-05-19 21:20:04 -0700

    pass wave_03 and wave_04

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -22,4 +22,23 @@ class Vendor:
                 matching_list.append(i)
         print("see",matching_list)
         return matching_list
+
+    def swap_items(self, friend, my_item, their_item):
+        if my_item in self.inventory and their_item in friend.inventory:
+            self.inventory.append(their_item)
+            self.inventory.remove(my_item)
+            friend.inventory.append(my_item)
+            friend.inventory.remove(their_item)
+            return True
+        
+        return False
+    def swap_first_item(self,friend):
+        if self.inventory and friend.inventory:
+            self.inventory.append(friend.inventory[0])
+            friend.inventory.remove(friend.inventory[0])
+            friend.inventory.append(self.inventory[0])
+            self.inventory.remove(self.inventory[0])
+           
+            return True
+        return False
     
