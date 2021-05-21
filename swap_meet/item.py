4d7a989cd671ea57fcc7eb224e28b78b6acf90bd
Andrea Valliere <amvalliere@gmail.com>
2021-05-20 23:18:48 -0700

    Adds Item child classes Clothing, Electronics, and Decor plus Item instance method condition_description() to pass wave 5 tests

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,6 +1,18 @@
 class Item:
-    def __init__(self, category = ''):
+    def __init__(self, category = '', condition = 0):
         self.category = category
+        self.condition = condition
 
     def __str__(self):
         return "Hello World!"
+
+    def condition_description(self):
+        condition_descriptions = {
+            0: "still has tags",
+            1: "mint",
+            2: "good",
+            3: "some signs of wear",
+            4: "as is",
+            5: "may actually be garbage"
+        }
+        return condition_descriptions[self.condition]
