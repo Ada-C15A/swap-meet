5520d9e4bb6874051b6841a301efe64701de8a82
clear <clear>
2021-05-22 12:51:07 -0700

    adds all passing tests from wave 1-6

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,15 @@
-class Clothing:
-    pass
+from typing import cast
+from .item import Item
+from swap_meet.item import Item
+
+class Clothing(Item):
+    def __init__(self, category="Clothing", condition=0):
+        super().__init__(category, condition)
+        # if condition == None:
+        #     super().__init__(category="Clothing")
+        # else:
+        #     super().__init__(category="Clothing", condition=None)
+    
+    def __str__(self): #overwritten the string method
+        return "The finest clothing you could wear."
+    
--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,13 @@
-class Item:
-    pass
+
+class Item():
+    def __init__(self, category="", condition=0):
+        self.category = category
+        self.condition = condition
+    
+    def __str__(self):
+        return "Hello World!"
+    
+    def condition_description(self):
+        item_descriptions = [ "Mint", "Excellent", "Good", "Fair", "Poor:", "Trash"]
+        item_descriptions = item_descriptions[::-1]
+        return item_descriptions[self.condition]
